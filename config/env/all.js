9ee580dc9e0bf8169a1a207f0f849fd09cc185d4
Stylll <stephen.aribaba@yahoo.com>
2018-06-25 17:09:35 +0100

    chore(travis): integrate travis ci - automate unit tests with travis ci - add travis build badge to readme file [Finishes #158487956]

--- config/env/all.js
+++ config/env/all.js
@@ -1,9 +1,22 @@
 var path = require('path'),
 rootPath = path.normalize(__dirname + '/../..');
 var keys = rootPath + '/keys.txt';
+require('dotenv').config();
 
-module.exports = {
-	root: rootPath,
-	port: process.env.PORT || 3000,
-    db: process.env.MONGOHQ_URL
-};
+var connectionConfig = {};
+
+if (process.env.NODE_ENV === 'test') {
+	connectionConfig = {
+		root: rootPath,
+		port: process.env.PORT || 3000,
+    	db: process.env.MONGOHQ_URL_TEST
+	}
+} else {
+	connectionConfig = {
+		root: rootPath,
+		port: process.env.PORT || 3000,
+    	db: process.env.MONGOHQ_URL
+	}
+}
+
+module.exports = connectionConfig;
