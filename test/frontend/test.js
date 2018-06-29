608e65d9a0b301b14e131fdc1af54df23e41d3f0
Otse Obande <otseobande@gmail.com>
2018-06-29 12:38:37 +0100

    chore(refactor): restructure codebase
    - delete test directory
    
    [Finishes #158687936]

--- test/frontend/test.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable */
-describe('Testing AngularJS Test Suite', function() {
-  describe('Testing Angular Karma.conf', function() {
-    it('should return true', function() {
-      expect(true).toBeTruthy();
-    });
-  });
-});
