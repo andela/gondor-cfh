f7a7ea7e824ab6ffb820d34f05abd4065a30c2e5
Ogooluwa Akinola <rovilayjnr01@gmail.com>
2018-07-03 08:41:12 +0100

    feature(signup): user recieve jwt on signup
    - update signup test
    - update eslint config
    
    [Update #158487962]

--- server/test/user/routes.js
+++ server/test/user/routes.js
@@ -10,7 +10,7 @@ chai.use(chaiHttp);
 
 describe('Users API Routes', () => {
   const newUser = {
-    name: 'myName',
+    name: 'myName ',
     email: 'test@test.com',
     username: 'myName006',
     password: 'password'
@@ -68,91 +68,6 @@ describe('Users API Routes', () => {
           expect(res.body.success).to.equal(false);
           expect(res.body.message).to.be.an('array');
           expect(res.body.message).to.be.contain('Password is required');
-
-          if (err) done(err);
-        });
-      done();
-    });
-  });
-
-  describe('Post /api/auth/login', () => {
-    before((done) => {
-      mongoose.connection.collections.users.drop((err) => {
-        if (err) return done(err);
-      });
-
-      // Add user to database
-      const user = new User({
-        name: 'myName',
-        email: 'test@test.com',
-        username: 'myName006',
-        password: 'password'
-      });
-
-      done();
-    });
-
-    // Add user to database
-    const user = {
-      email: 'test@test.com',
-      password: 'password'
-    };
-
-    it('should log user in successfully and return token', (done) => {
-      chai.request(server.listen())
-        .post('/api/auth/login')
-        .send(user)
-        .end((err, res) => {
-          expect(res.status).to.equal(200);
-          expect(res.body.message).to.equal('Login successful!');
-          expect(res.body).to.have.property('token');
-
-          if (err) done(err);
-        });
-      done();
-    });
-
-    it('should return error if password is incorrect', (done) => {
-      user.email = 'nottest@test.com';
-      chai.request(server.listen())
-        .post('/api/auth/login')
-        .send(user)
-        .end((err, res) => {
-          expect(res.status).to.equal(404);
-          expect(res.body.success).to.equal(false);
-          expect(res.body.message).to.be.equal('User not found!');
-
-          if (err) done(err);
-        });
-      done();
-    });
-
-    it('should return error if password is incorrect', (done) => {
-      user.email = 'test@test.com';
-      user.password = '123456';
-      chai.request(server.listen())
-        .post('/api/auth/login')
-        .send(user)
-        .end((err, res) => {
-          expect(res.status).to.equal(400);
-          expect(res.body.success).to.equal(false);
-          expect(res.body.message).to.be.equal('Password incorrect!');
-
-          if (err) done(err);
-        });
-      done();
-    });
-
-    it('should return error if fields are empty', (done) => {
-      user.email = '';
-      user.password = 'password';
-      chai.request(server.listen())
-        .post('/api/auth/login')
-        .send(user)
-        .end((err, res) => {
-          expect(res.status).to.equal(400);
-          expect(res.body.success).to.equal(false);
-          expect(res.body.message).to.be.equal('You must fill all fields.');
 
           if (err) done(err);
         });
